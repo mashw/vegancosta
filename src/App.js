2ae480ab60491d120cb764c939d02a0492d41d10
randomstuffgenerator <mashwilliams@gmail.com>
2020-07-21 20:23:58 +0100

    Added loading spinner.

--- src/App.js
+++ src/App.js
@@ -10,201 +10,201 @@ import { data } from './data/data';
 import { Card } from './components/card';
 
 function App() {
-	const [ cards, setCards ] = useState([]);
+  const [ cards, setCards ] = useState([]);
+  const [ spinner, setSpinner ] = useState([]);
 	let timeout;
 
 	const debounce = (e) => {
+    setCards([]);
+    setSpinner(true);    
 		clearTimeout(timeout);
 		setTimeout(() => {
 			getResults();
-		}, 1400);
+		}, 1000);
 	};
 
-	const getResults = () => {
+	const getResults = () => {    
 		const searchTerm = document.getElementsByClassName('search')[0].value;
 		let filteredArr = [];
 		let elements = [];
 
 		if (searchTerm.length > 0) {
-      
-      const filterByValue = data.filter(function(obj) {
-        const terms = searchTerm.toLowerCase().split(' ');
-        let flag = true;
-        for (var i=0; i < terms.length; i++) {
-          if (!obj["description"].toLowerCase().includes(terms[i]) && !obj["category"].toLowerCase().includes(terms[i])) {
-            flag = false;
-          }
-        }
-        if (flag) {
-          return true;
-        }
-        
-      });
+      //takes the search term and checks each object for a match, if found it is added to filteredArr and then sorted and a set of "cards" is generated and set as a state.
+			const filterByValue = data.filter(function(obj) {
+				const terms = searchTerm.toLowerCase().split(' ');
+				let flag = true;
+				for (var i = 0; i < terms.length; i++) {
+					if (
+            //if the description or the category don't contain the term return false
+						!obj['description'].toLowerCase().includes(terms[i]) &&
+						!obj['category'].toLowerCase().includes(terms[i])
+					) {
+						flag = false;
+					}
+				}
+				if (flag) {
+          //if the description or the category contain the term return true
+					return true;
+				}
+			});
 
 			filteredArr.push(filterByValue);
-
 			filteredArr[0].sort((a, b) => a.description.localeCompare(b.description));
-
 			filteredArr[0].sort(sortByVegan);
 
 			for (var i = 0; i < filteredArr[0].length; i++) {
 				elements.push(<Card value={filteredArr[0][i]} key={uuid()} indexKey={i} />);
 			}
-
+      setSpinner(false);
 			setCards(elements);
-    }
-    
+		}
+
 		if (searchTerm === '') {
+      setSpinner(false);
 			filteredArr = [];
 			elements = [];
 			setCards([]);
 		}
-  };
+	};
 
-  const getHotDrinks = (e) => {
-	e.preventDefault();
-	console.log("Hot drinks");
-	setCards([]);
+	const getHotDrinks = (e) => {
+		e.preventDefault();
+		console.log('Hot drinks');
+		setCards([]);
 
-	let filteredArr = [];
-	let elements = [];
-	const terms = ["Hot Chocolate", "Coffee", "Tea", "Hot Drink"];
-	const filterByValue = data.filter(function(obj) {
-        let flag = true;
-		if (!terms.some(x => obj["category"].includes(x))) {
-			flag = false;			
-        }
-        if (flag) {
-          return true;
-        }        
-	  });
-
-	  
-
-	  filteredArr.push(filterByValue);
-
-			filteredArr[0].sort((a, b) => a.description.localeCompare(b.description));
-
-			filteredArr[0].sort(sortByVegan);
-
-			for (var i = 0; i < filteredArr[0].length; i++) {
-				elements.push(<Card value={filteredArr[0][i]} key={uuid()} indexKey={i} />);
+		let filteredArr = [];
+		let elements = [];
+		const terms = [ 'Hot Chocolate', 'Coffee', 'Tea', 'Hot Drink' ];
+		const filterByValue = data.filter(function(obj) {
+			let flag = true;
+			if (!terms.some((x) => obj['category'].includes(x))) {
+				flag = false;
 			}
-
-			setCards(elements);
-  };
-
-  const getColdDrinks = (e) => {
-	e.preventDefault();
-	data.forEach((x) => console.log(x["category"]));
-	setCards([]);
-
-	let filteredArr = [];
-	let elements = [];
-	const terms = ["Cold Drinks"];
-	const filterByValue = data.filter(function(obj) {
-        let flag = true;
-		if (!terms.some(x => obj["category"].includes(x))) {
-			flag = false;			
-        }
-        if (flag) {
-          return true;
-        }        
-	  });
-
-	  
-
-	  filteredArr.push(filterByValue);
-
-			filteredArr[0].sort((a, b) => a.description.localeCompare(b.description));
-
-			filteredArr[0].sort(sortByVegan);
-
-			for (var i = 0; i < filteredArr[0].length; i++) {
-				elements.push(<Card value={filteredArr[0][i]} key={uuid()} indexKey={i} />);
+			if (flag) {
+				return true;
 			}
+		});
 
-			setCards(elements);
-  };
+		filteredArr.push(filterByValue);
+		filteredArr[0].sort((a, b) => a.description.localeCompare(b.description));
+		filteredArr[0].sort(sortByVegan);
 
-  const getCakes = (e) => {
-	e.preventDefault();
-	setCards([]);
+		for (var i = 0; i < filteredArr[0].length; i++) {
+			elements.push(<Card value={filteredArr[0][i]} key={uuid()} indexKey={i} />);
+		}
 
-	let filteredArr = [];
-	let elements = [];
-	const terms = ["Sweet Pastries", "Cakes"];
-	const filterByValue = data.filter(function(obj) {
-        let flag = true;
-		if (!terms.some(x => obj["category"].includes(x))) {
-			flag = false;			
-        }
-        if (flag) {
-          return true;
-        }        
-	  });
+		setCards(elements);
+	};
 
-	  
+	const getColdDrinks = (e) => {
+		e.preventDefault();
+		data.forEach((x) => console.log(x['category']));
+		setCards([]);
 
-	  filteredArr.push(filterByValue);
-
-			filteredArr[0].sort((a, b) => a.description.localeCompare(b.description));
-
-			filteredArr[0].sort(sortByVegan);
-
-			for (var i = 0; i < filteredArr[0].length; i++) {
-				elements.push(<Card value={filteredArr[0][i]} key={uuid()} indexKey={i} />);
+		let filteredArr = [];
+		let elements = [];
+		const terms = [ 'Cold Drinks' ];
+		const filterByValue = data.filter(function(obj) {
+			let flag = true;
+			if (!terms.some((x) => obj['category'].includes(x))) {
+				flag = false;
 			}
-
-			setCards(elements);
-  };
-
-  const getFood = (e) => {
-	e.preventDefault();
-	setCards([]);
-
-	let filteredArr = [];
-	let elements = [];
-	const terms = ["Snacks", "Sandwiches", "Salads", "Panini", "Toasties", "Hot Meals", "Breakfast", "Savoury Pastries"];
-	const filterByValue = data.filter(function(obj) {
-        let flag = true;
-		if (!terms.some(x => obj["category"].includes(x))) {
-			flag = false;			
-        }
-        if (flag) {
-          return true;
-        }        
-	  });
-
-	  
-
-	  filteredArr.push(filterByValue);
-
-			filteredArr[0].sort((a, b) => a.description.localeCompare(b.description));
-
-			filteredArr[0].sort(sortByVegan);
-
-			for (var i = 0; i < filteredArr[0].length; i++) {
-				elements.push(<Card value={filteredArr[0][i]} key={uuid()} indexKey={i} />);
+			if (flag) {
+				return true;
 			}
+		});
 
-			setCards(elements);
-  };
+		filteredArr.push(filterByValue);
+		filteredArr[0].sort((a, b) => a.description.localeCompare(b.description));
+		filteredArr[0].sort(sortByVegan);
 
-  
-  
+		for (var i = 0; i < filteredArr[0].length; i++) {
+			elements.push(<Card value={filteredArr[0][i]} key={uuid()} indexKey={i} />);
+		}
+
+		setCards(elements);
+	};
+
+	const getCakes = (e) => {
+		e.preventDefault();
+		setCards([]);
+
+		let filteredArr = [];
+		let elements = [];
+		const terms = [ 'Sweet Pastries', 'Cakes' ];
+		const filterByValue = data.filter(function(obj) {
+			let flag = true;
+			if (!terms.some((x) => obj['category'].includes(x))) {
+				flag = false;
+			}
+			if (flag) {
+				return true;
+			}
+		});
+
+		filteredArr.push(filterByValue);
+		filteredArr[0].sort((a, b) => a.description.localeCompare(b.description));
+		filteredArr[0].sort(sortByVegan);
+
+		for (var i = 0; i < filteredArr[0].length; i++) {
+			elements.push(<Card value={filteredArr[0][i]} key={uuid()} indexKey={i} />);
+		}
+
+		setCards(elements);
+	};
+
+	const getFood = (e) => {
+		e.preventDefault();
+		setCards([]);
+
+		let filteredArr = [];
+		let elements = [];
+		const terms = [
+			'Snacks',
+			'Sandwiches',
+			'Salads',
+			'Panini',
+			'Toasties',
+			'Hot Meals',
+			'Breakfast',
+			'Savoury Pastries'
+		];
+		const filterByValue = data.filter(function(obj) {
+			let flag = true;
+			if (!terms.some((x) => obj['category'].includes(x))) {
+				flag = false;
+			}
+			if (flag) {
+				return true;
+			}
+		});
+
+		filteredArr.push(filterByValue);
+
+		filteredArr[0].sort((a, b) => a.description.localeCompare(b.description));
+
+		filteredArr[0].sort(sortByVegan);
+
+		for (var i = 0; i < filteredArr[0].length; i++) {
+			elements.push(<Card value={filteredArr[0][i]} key={uuid()} indexKey={i} />);
+		}
+
+		setCards(elements);
+	};
+
 	return (
 		<div className="Vegan Costa Coffee">
 			<Header />
 			<div className="main-content container">
-				<Searchbox 
-					debounce={debounce} 
-					getHotDrinks={getHotDrinks} 
-					getColdDrinks={getColdDrinks}	
+				<Searchbox
+					debounce={debounce}
+					getHotDrinks={getHotDrinks}
+					getColdDrinks={getColdDrinks}
 					getCakes={getCakes}
 					getFood={getFood}
 				/>
 				<div className="card-container">
+          {spinner === true ? <img className="loading-spinner" src={require('../src/imgs/loading.gif')} alt="Loading"></img> : <span></span>}
 					{cards}
 					{/* {trail.map(({ x, height, ...rest }, index) => (
           <animated.div
