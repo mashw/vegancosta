fc0ed082eac1fd933ce05ef90652f981f1afc10f
randomstuffgenerator <mashwilliams@gmail.com>
2020-07-28 16:57:29 +0100

    Updates

---
+++ precache-manifest.dbe92e2739d1fd6b931f868c4e578ae9.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284028471f184544fec2b49ad76fb317",
+    "url": "/vegancosta/index.html"
+  },
+  {
+    "revision": "be0d250915388956c4c4",
+    "url": "/vegancosta/static/css/main.ed9e0c45.chunk.css"
+  },
+  {
+    "revision": "853d0fccdbafb9e733b4",
+    "url": "/vegancosta/static/js/2.81f75995.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vegancosta/static/js/2.81f75995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0d250915388956c4c4",
+    "url": "/vegancosta/static/js/main.acf89fe7.chunk.js"
+  },
+  {
+    "revision": "d05c1f48c4b7cf595834",
+    "url": "/vegancosta/static/js/runtime-main.ba1b97ee.js"
+  },
+  {
+    "revision": "62be9424b402b7425ed6f6704ebfa78e",
+    "url": "/vegancosta/static/media/BrandonGrotesque-Bold.62be9424.woff"
+  },
+  {
+    "revision": "ce090b617751e0d5f84a22cad471585c",
+    "url": "/vegancosta/static/media/BrandonGrotesque-Regular.ce090b61.woff"
+  },
+  {
+    "revision": "574cde950a98cbe404827d388cb5e528",
+    "url": "/vegancosta/static/media/ClarendonRegular.574cde95.woff"
+  },
+  {
+    "revision": "5cef5234f71bb05185752797b9a2443e",
+    "url": "/vegancosta/static/media/breakfast.5cef5234.svg"
+  },
+  {
+    "revision": "61327d2d0a962047e81d637b69949b20",
+    "url": "/vegancosta/static/media/cakes.61327d2d.svg"
+  },
+  {
+    "revision": "e71dbd449b25b6747c277e62a759b8d3",
+    "url": "/vegancosta/static/media/coffee.e71dbd44.svg"
+  },
+  {
+    "revision": "242f3b3ad6be48318a99b76f08b69ebb",
+    "url": "/vegancosta/static/media/colddrinks.242f3b3a.svg"
+  },
+  {
+    "revision": "0d9edc14307a6def5719cabfeb29c320",
+    "url": "/vegancosta/static/media/cubes2.0d9edc14.jpg"
+  },
+  {
+    "revision": "53b32552495d70753a96f7990ff9a59b",
+    "url": "/vegancosta/static/media/extras.53b32552.svg"
+  },
+  {
+    "revision": "6570578bbd4504d08094e435e6210084",
+    "url": "/vegancosta/static/media/fruit.6570578b.svg"
+  },
+  {
+    "revision": "3e5bb14f3b2f5060133f0e0715e7910c",
+    "url": "/vegancosta/static/media/hotchocolate.3e5bb14f.svg"
+  },
+  {
+    "revision": "862f3a4b994386b102713fe88a8ce145",
+    "url": "/vegancosta/static/media/hotdrink.862f3a4b.svg"
+  },
+  {
+    "revision": "1036cf8f68168dfa0c9bc677ab55f5a6",
+    "url": "/vegancosta/static/media/hotmeals.1036cf8f.svg"
+  },
+  {
+    "revision": "750fbb6ab50296fe346a7dbe51234e01",
+    "url": "/vegancosta/static/media/loading.750fbb6a.gif"
+  },
+  {
+    "revision": "0b7f8f11be3e281c93e3284a0ef280fe",
+    "url": "/vegancosta/static/media/logo.0b7f8f11.svg"
+  },
+  {
+    "revision": "ab44725d1f632d6fe61e6e4cc864dc52",
+    "url": "/vegancosta/static/media/panini.ab44725d.svg"
+  },
+  {
+    "revision": "32c89c11c769d8a3468b423d23e67a27",
+    "url": "/vegancosta/static/media/question.32c89c11.svg"
+  },
+  {
+    "revision": "56bac1f337b34199901a7a5a45f564c4",
+    "url": "/vegancosta/static/media/salads.56bac1f3.svg"
+  },
+  {
+    "revision": "57ae3c31b612b2f5f77e9e3b0ff63d4e",
+    "url": "/vegancosta/static/media/sandwiches.57ae3c31.svg"
+  },
+  {
+    "revision": "55dc175838068e236c67cbdca4fa593b",
+    "url": "/vegancosta/static/media/savourypastries.55dc1758.svg"
+  },
+  {
+    "revision": "4cfa705d14e158d5d7fc2a7dc433ff2e",
+    "url": "/vegancosta/static/media/seal.4cfa705d.svg"
+  },
+  {
+    "revision": "7a63b2e2953eee50d8e3330756307b5f",
+    "url": "/vegancosta/static/media/snacks.7a63b2e2.svg"
+  },
+  {
+    "revision": "55dc175838068e236c67cbdca4fa593b",
+    "url": "/vegancosta/static/media/sweetpastries.55dc1758.svg"
+  },
+  {
+    "revision": "f081816de0ed94da5b79e740f4b7edcd",
+    "url": "/vegancosta/static/media/tea.f081816d.svg"
+  },
+  {
+    "revision": "ab44725d1f632d6fe61e6e4cc864dc52",
+    "url": "/vegancosta/static/media/toasties.ab44725d.svg"
+  },
+  {
+    "revision": "947df5fd2c71a35d4b96ef6243504774",
+    "url": "/vegancosta/static/media/x.947df5fd.svg"
+  }
+]);
