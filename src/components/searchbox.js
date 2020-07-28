993e3157c7e2d8f26d46dad97de86ab8d4b383ef
randomstuffgenerator <mashwilliams@gmail.com>
2020-07-28 16:52:32 +0100

    Adjusted button colours and made the size and width of the buttons dynamic depending on width of page.

--- src/components/searchbox.js
+++ src/components/searchbox.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import  React, { useState, useEffect } from 'react';
 
 const Searchbox = (props) => {
+
+    const [windowWidth, setWindowWidth] = useState([window.innerWidth]);
+
+    useEffect(() => {
+      const handleResize = () => {
+        const newWidth = window.innerWidth;
+        if (newWidth < 430) {
+          console.log("smaller than 930");
+          let buttonWrapper = document.getElementById("buttons");
+          buttonWrapper.classList.add("wider-wrapper");
+        }
+        else {
+          console.log("larger or equal than 930");
+          let buttonWrapper = document.getElementById("buttons");
+          buttonWrapper.classList.remove("wider-wrapper");
+        }
+        setWindowWidth(newWidth);
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => window.removeEventListener('resize', handleResize);
+    });
+
+
     return (
         <div className="searchbox">
         
@@ -11,21 +36,21 @@ const Searchbox = (props) => {
                     <img src={require('../imgs/search.svg')} width="170" height="95" alt="Vegan Costa" />
                 </button> */}
                 <p className="category-text">Or choose a category to show a list of available products:</p>
-                <div className="filter-buttons-wrapper">
+                <div className="filter-buttons-wrapper" id="buttons">
                     <button className="filter hot-drinks" onClick={props.getHotDrinks}>
-                        Hot Drinks
+                        {windowWidth >= 930 ?  "Hot Drinks" : "!"}
                     </button>
                     <button className="filter cold-drinks" onClick={props.getColdDrinks}>
-                        Cold drinks
+                        {windowWidth >= 930 ?  "Cold Drinks" : "!"}
                     </button>
                     <button className="filter cakes" onClick={props.getCakes}>
-                        Cakes
+                        {windowWidth >= 930 ?  "Cakes" : "!"}
                     </button>
                     <button className="filter food" onClick={props.getFood}>
-                        Food
+                        {windowWidth >= 930 ?  "Food" : "!"}
                     </button>
                     <button className="filter custom-drinks">
-                        Custom drinks!
+                        {windowWidth >= 930 ?  "Custom Drinks" : "!"}
                     </button>      
                 </div>
             </form>
